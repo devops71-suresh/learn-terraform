2781e920d78524785ff78763e36bba6570c6b711
Suresh <bsuresh4589@gmail.com>
2023-03-12 19:55:19 +0530

    add

--- 01-sample/test.tf
+++ 01-sample/test.tf
@@ -1,9 +1,8 @@
 resource "aws_instance" "ec2" {
-
-  ami =   "ami-0a017d8ceb274537d"
+  ami = "ami-0a017d8ceb274537d"
   instance_type = "t3.micro"
-  security_groups = [ "sg-03d0dd2c9b54dc2af" ]
+  vpc_security_group_ids = ["sg-03d0dd2c9b54dc2af"]
   tags = {
-    Name = "Demo"
+    Name = "test2"
   }
 }

b6a58f21b597851ba90b66f43507d8b3813e91b4
Suresh <bsuresh4589@gmail.com>
2023-03-12 18:45:52 +0530

    add first commit

---
+++ 01-sample/test.tf
@@ -0,0 +1,8 @@
+resource "aws_instance" "ec2" {
+  ami =   "ami-0a017d8ceb274537d"
+  instance_type = "t3.micro"
+  security_groups = [ "sg-03d0dd2c9b54dc2af" ]
+  tags = {
+    Name = "Demo"
+  }
+}

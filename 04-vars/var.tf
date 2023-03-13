1b3c740e56087dd2de156498f98e79c341edc606
Suresh <bsuresh4589@gmail.com>
2023-03-13 11:54:53 +0530

    var list and map commits

--- 04-vars/var.tf
+++ 04-vars/var.tf
@@ -10,3 +10,22 @@ variable "sample2" {}
 output "sample2" {
   value = var.sample2
 }
+
+variable "sample3" {
+  default = "Hello"
+}
+
+variable "sample4" {
+  default = ["lak",45,true]
+}
+
+variable "sample5" {
+  default = {
+    name    = "suresh"
+    no      = 100
+    boolean = false
+  }
+}
+
+output "types" {
+  value = "variable sample3 -${var.sample3},Second value of List - ${var.sample4[1]},Third value of Map - ${var.sample5[boolean]}"}
